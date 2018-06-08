05129af0fd60e8732f5fc989bce85f5f925ef2a2
Herman Rogers <herman.rogers@gmail.com>
2018-06-08 15:22:30 +0200

    Add relative font imports

--- src/fonts/fonts.style.js
+++ src/fonts/fonts.style.js
@@ -1,4 +1,11 @@
 import { injectGlobal } from 'styled-components';
+import woff200 from '../../public/fonts/35C396_0_0.woff2';
+import ie9Compat40 from '../../public/fonts/35C396_4_0.eot';
+import woff240 from '../../public/fonts/35C396_4_0.woff2';
+import woff40 from '../../public/fonts/35C396_4_0.woff';
+import tt40 from '../../public/fonts/35C396_4_0.ttf';
+import svg40 from '../../public/fonts/35C396_4_0.svg';
+import woff250 from '../../public/fonts/35C396_5_0.woff2';
 
 // eslint-disable-next-line
 injectGlobal`
@@ -6,20 +13,18 @@ injectGlobal`
     font-family: 'Norms';
     font-weight: 300;
     font-style: normal;
-    src: url('/fonts/35C396_4_0.eot'); /* IE9 Compat Modes */
-    src: url('/fonts/35C396_4_0.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
-         url('/fonts/35C396_4_0.woff2') format('woff2'), /* Super Modern Browsers */
-         url('/fonts/35C396_4_0.woff') format('woff'), /* Pretty Modern Browsers */
-         url('/fonts/35C396_4_0.ttf')  format('truetype'),
-         url('/fonts/35C396_4_0.svg#svgFontName') format('svg'); /* Legacy iOS */
+    src: url(${ie9Compat40}); /* IE9 Compat Modes */
+    src: url(${woff240}) format('woff2'), /* Super Modern Browsers */
+         url(${woff40}) format('woff'), /* Pretty Modern Browsers */
+         url(${tt40})  format('truetype'),
+         url(${svg40}) format('svg'); /* Legacy iOS */
     }
     @font-face {
       font-family: 'Norms';
       font-weight: 300;
       font-style: normal;
       src: url('/fonts/35C396_2_0.eot'); /* IE9 Compat Modes */
-      src: url('/fonts/35C396_2_0.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
-           url('/fonts/35C396_2_0.woff2') format('woff2'), /* Super Modern Browsers */
+      src: url('/fonts/35C396_2_0.woff2') format('woff2'), /* Super Modern Browsers */
            url('/fonts/35C396_2_0.woff') format('woff'), /* Pretty Modern Browsers */
            url('/fonts/35C396_2_0.ttf')  format('truetype'),
            url('/fonts/35C396_2_0.svg#svgFontName') format('svg'); /* Legacy iOS */
@@ -30,7 +35,7 @@ injectGlobal`
       font-style: normal;
       src: url('/fonts/35C396_5_0.eot'); /* IE9 Compat Modes */
       src: url('/fonts/35C396_5_0.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
-           url('/fonts/35C396_5_0.woff2') format('woff2'), /* Super Modern Browsers */
+           url(${woff250}) format('woff2'), /* Super Modern Browsers */
            url('/fonts/35C396_5_0.woff') format('woff'), /* Pretty Modern Browsers */
            url('/fonts/35C396_5_0.ttf')  format('truetype'),
            url('/fonts/35C396_5_0.svg#svgFontName') format('svg'); /* Legacy iOS */
@@ -64,7 +69,7 @@ injectGlobal`
       font-style: normal;
       src: url('/fonts/35C396_0_0.eot'); /* IE9 Compat Modes */
       src: url('/fonts/35C396_0_0.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
-           url('/fonts/35C396_0_0.woff2') format('woff2'), /* Super Modern Browsers */
+           url(${woff200}) format('woff2'), /* Super Modern Browsers */
            url('/fonts/35C396_0_0.woff') format('woff'), /* Pretty Modern Browsers */
            url('/fonts/35C396_0_0.ttf')  format('truetype'),
            url('/fonts/35C396_0_0.svg#svgFontName') format('svg'); /* Legacy iOS */
