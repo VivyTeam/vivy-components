dbe69936d202b29040b019107d6788ab144d1959
Herman Rogers <herman.rogers@protonmail.com>
2018-08-08 18:42:54 +0200

    Update typography

--- src/theme/ThemeFonts.jsx
+++ src/theme/ThemeFonts.jsx
@@ -1,37 +1,61 @@
 import React from 'react';
-import ThemeStyles from './theme.style';
+import FontStyles from './font.style';
 
 export default function ThemeFonts() {
   return (
-    <ThemeStyles>
-      <div className="font-container">
-        <h1>Typography</h1>
-        <p id="alt">
-          We use TT Norms, giving text in Vivy a fun and friendly feel to match
-          our illustrations.
+    <FontStyles>
+      <div className="container">
+        <div className="title">Typography</div>
+        <br />
+        <h1>Heading One</h1>
+        <small>TTNorms-Medium, 3.5rem (56px), #575756, line-height: 64px</small>
+        <br />
+        <h2>Heading Two</h2>
+        <small>TTNorms-Regular, 3rem (48px), #575756, line-height: 60px</small>
+        <br />
+        <h3>Heading Three</h3>
+        <small>
+          TTNorms-Regular, 2.5rem (40px), #575756, line-height: 52px
+        </small>
+        <br />
+        <h4>Heading Four</h4>
+        <small>TTNorms-Regular, 2rem (32px), #575756, line-height: 40px</small>
+        <br />
+        <h4>Heading Five</h4>
+        <small>TTNorms-Medium, 1.5rem (24px), #575756, line-height: 32px</small>
+        <br />
+        <h4>Heading Six</h4>
+        <small>
+          TTNorms-Medium, 1.25rem (20px), #575756, line-height: 20px
+        </small>
+        <br />
+        <div className="small-title">Paragraph</div>
+        <p>
+          Teilen Sie Untersuchungs­ergebnisse und Befunde sicher und einfach mit
+          Patienten und erhalten Sie in der gleichen Weise wichtige Unterlagen
+          aus vorangegangenen Untersuchungen. Für eine bessere Patientenbindung,
+          effiziente Diagnosen und weniger Doppelbehandlungen.
         </p>
+        <small>TTNorms-Regular, 1rem (16px), #575756, line-height: 24px</small>
         <br />
-        <h1>&lt;h1&gt; Headline - 1.5em 600 bold</h1>
-        <h1>The quick brown fox jumps over the lazy dog</h1>
+        <div className="small-title">Paragraph Big</div>
+        <p className="paragraph-big">
+          Teilen Sie Untersuchungs­ergebnisse und Befunde sicher und einfach mit
+          Patienten und erhalten Sie in der gleichen Weise wichtige Unterlagen
+          aus vorangegangenen Untersuchungen. Für eine bessere Patientenbindung,
+          effiziente Diagnosen und weniger Doppelbehandlungen.
+        </p>
+        <small>TTNorms-Regular, 1rem (16px), #575756, line-height: 26px</small>
         <br />
-        <h2>&lt;h2&gt; Title - 1.25em 600 bold</h2>
-        <h2>The quick brown fox jumps over the lazy dog</h2>
+        <div className="caption">Caption</div>
+        <small>
+          TTNorms-Regular, 0.9375rem (15px), #575756, line-height: 20px
+        </small>
         <br />
-        <h3>&lt;h3&gt; Subheading - 1em 600 bold</h3>
-        <h3>The quick brown fox jumps over the lazy dog</h3>
+        <div className="link">This is a link</div>
+        <small>TTNorms-Medium, 1rem (16px), #575756, line-height: 26px</small>
         <br />
-        <h3 id="alt">&lt;h3&gt; Subheading alt - 1em 100 regular</h3>
-        <h3 id="alt">The quick brown fox jumps over the lazy dog</h3>
-        <br />
-        <p>&lt;p&gt; Body - 0.93em 600 bold</p>
-        <p>The quick brown fox jumps over the lazy dog</p>
-        <br />
-        <p id="alt">&lt;p&gt; Body alt - 0.93em 100 regular</p>
-        <p id="alt">The quick brown fox jumps over the lazy dog</p>
-        <br />
-        <p className="caption">&lt;p&gt; Caption - 0.81em 400 medium</p>
-        <p className="caption">The quick brown fox jumps over the lazy dog</p>
       </div>
-    </ThemeStyles>
+    </FontStyles>
   );
 }
---
+++ src/theme/font.style.js
@@ -0,0 +1,163 @@
+import styled from 'styled-components';
+
+const FontStyles = styled.div`
+  h1 {
+    font-size: 3.5rem;
+    font-weight: 500;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.14;
+    letter-spacing: normal;
+    color: #575756;
+    margin: 6px 0;
+  }
+
+  h2 {
+    font-size: 3rem;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.25;
+    letter-spacing: normal;
+    color: #575756;
+    margin: 6px 0;
+  }
+
+  h3 {
+    font-size: 2.5rem;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.3;
+    letter-spacing: normal;
+    color: #575756;
+    margin: 6px 0;
+  }
+
+  h4 {
+    font-size: 2rem;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.25;
+    letter-spacing: normal;
+    color: #575756;
+    margin: 6px 0;
+  }
+
+  h5 {
+    font-size: 1.5rem;
+    font-weight: 500;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.33;
+    letter-spacing: normal;
+    color: #575756;
+    margin: 6px 0;
+  }
+
+  h6 {
+    font-size: 1.25rem;
+    font-weight: 500;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.4;
+    letter-spacing: normal;
+    color: #575756;
+    margin: 6px 0;
+  }
+
+  small {
+    font-size: 13px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.54;
+    letter-spacing: normal;
+    color: #898988;
+  }
+
+  p {
+    font-size: 1rem;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.44;
+    letter-spacing: normal;
+    color: #575756;
+  }
+
+  .paragraph-big {
+    font-size: 1rem;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.44;
+    letter-spacing: normal;
+    color: #575756;
+  }
+
+  .paragraph {
+    font-size: 1rem;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.44;
+    letter-spacing: normal;
+    color: #575756;
+  }
+
+  br {
+    margin-bottom: 35px;
+  }
+
+  .container {
+    margin: 80px 0 50px 110px;
+    max-width: 600px;
+  }
+
+  .title {
+    font-size: 22px;
+    font-weight: 500;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 0.91;
+    letter-spacing: normal;
+    color: #898988;
+    text-transform: uppercase;
+  }
+
+  .small-title {
+    font-size: 16px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.5;
+    letter-spacing: normal;
+    color: #575756;
+  }
+
+  .caption {
+    font-size: 0.9375rem;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.73;
+    letter-spacing: normal;
+    color: #898988;
+    margin-bottom: 16px;
+  }
+
+  .link {
+    font-size: 15px;
+    font-weight: 500;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.47;
+    letter-spacing: normal;
+    color: #00bfac;
+    margin-bottom: 16px;
+  }
+`;
+
+export default FontStyles;
