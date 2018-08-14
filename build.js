073f9b932b18542fca9095ba529049ba871bcdbc
Herman Rogers <herman.rogers@protonmail.com>
2018-08-14 15:58:10 +0200

    Update build command to publish and update tags

--- build.js
+++ build.js
@@ -1,10 +1,18 @@
 /* eslint no-console: 0 */
 const util = require('util');
-const exec = util.promisify(require('child_process').execSync);
+const exec = util.promisify(require('child_process').exec);
 const { version } = require('./package.json');
 
-const env = process.argv[2];
+const testEnv = process.argv[2] === 'test';
 
+function compareVersions(first, second) {
+  const verOne = first.replace(/\./g, '');
+  const verTwo = second.replace(/\./g, '');
+
+  return parseInt(verOne, 10) > parseInt(verTwo, 10);
+}
+
+// Run a chain of commands within a single process
 async function runCommands(commands) {
   try {
     const combinedCommands = commands.reduce(
@@ -15,20 +23,29 @@ async function runCommands(commands) {
 
     console.log(`Command "${combinedCommands}" completed. Output:\n${stdout}`);
     console.log(stderr);
+
+    return stdout;
   } catch (err) {
     console.error(err);
+    throw new Error(err);
   }
 }
 
 async function publishPackage() {
-  const build = 'npm run build:package';
+  console.log('Publishing package to npm');
 
-  await runCommands([build]);
+  const build = 'npm run build:package';
+  const prepPackage =
+    'cp package.json README.md index.js ./build && cd ./build';
+  const npmPublish = testEnv ? 'npm pack' : 'npm publish --access public';
+
+  await runCommands([build, prepPackage, npmPublish]);
 }
 
 // Build storybook website and push it to github pages
 async function updateWebsite() {
   console.log('Updating static website');
+
   const buildStorybook = 'npm run build:storybook';
   const stageStaticWebsite = 'mkdir .static-website && cd .static-website';
   const clone =
@@ -44,14 +61,27 @@ async function updateWebsite() {
     updateStaticFiles,
   ];
 
-  if (env !== 'test') commands.push(pushToWebsite);
+  if (testEnv) commands.push(pushToWebsite);
 
   await runCommands(commands);
 }
 
+async function updateGitTag() {
+  if (testEnv) return null;
+  return runCommands([`git tag ${version}`, `git push origin ${version}`]);
+}
+
 (async function runBuildProcess() {
   console.log(`Running build process for vivy-components ${version}`);
+  const tag = await runCommands(['git describe --abbrev=0 --tags']);
 
-  await publishPackage();
-  // await updateWebsite();
+  // Check if the version has be incremented
+  // if it has deploy package and update git tag
+  const versionIncremented = compareVersions(version, tag);
+  if (versionIncremented) {
+    console.log(`${tag} has been incremented to ${version}, deploying package`);
+    await publishPackage();
+    await updateGitTag();
+  }
+  await updateWebsite();
 })();
--- src/exports.js
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './button/Button';
-export { default as FlexGrid } from './grid/FlexGrid';
-export { default as Row } from './grid/Row';
-export { default as Col } from './grid/Col';
