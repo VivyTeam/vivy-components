a51241a0304db1ce096a21c45184891b6248af4a
Herman Rogers <herman.rogers@protonmail.com>
2018-08-15 18:23:21 +0200

    Add withInfo addon

--- stories/icons.story.jsx
+++ stories/icons.story.jsx
@@ -1,32 +1,44 @@
 import React from 'react';
-import { storiesOf, setAddon } from '@storybook/react';
-import JSXAddon from 'storybook-addon-jsx';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 import styled from 'styled-components';
 import Icon from '../src/icons/Icon';
 
 const IconsStory = styled.div`
-  display: flex;
-  flex-direction: row;
+  .container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin-top: 200px;
+  }
+
+  i {
+    font-size: 2em;
+    margin: 10px;
+  }
 `;
 
-setAddon(JSXAddon);
-
-storiesOf('Icons', module).addWithJSX('Primary', () => (
-  <IconsStory>
-    <Icon name="blood-type" />
-    <Icon name="call-contact" />
-    <Icon name="contact-info" />
-    <Icon name="doctor-info" />
-    <Icon name="gender-female" />
-    <Icon name="gender-male" />
-    <Icon name="gender-other" />
-    <Icon name="medical-health" />
-    <Icon name="medical-id" />
-    <Icon name="medication-info" />
-    <Icon name="organ-donor" />
-    <Icon name="warning-sign" />
-    <Icon name="weight-info" />
-    <Icon name="will-information" />
-    <Icon name="height-icon" />
-  </IconsStory>
-));
+storiesOf('Icons', module).add(
+  'Vivy Icons',
+  withInfo('Impact')(() => (
+    <IconsStory>
+      <div className="container">
+        <Icon name="blood-type" />
+        <Icon name="call-contact" />
+        <Icon name="contact-info" />
+        <Icon name="doctor-info" />
+        <Icon name="gender-female" />
+        <Icon name="gender-male" />
+        <Icon name="gender-other" />
+        <Icon name="medical-health" />
+        <Icon name="medical-id" />
+        <Icon name="medication-info" />
+        <Icon name="organ-donor" />
+        <Icon name="warning-sign" />
+        <Icon name="weight-info" />
+        <Icon name="will-information" />
+        <Icon name="height-icon" />
+      </div>
+    </IconsStory>
+  ))
+);
