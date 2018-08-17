085359b368fff18609b19c6b9a3a6dd82c85d894
Herman Rogers <herman.rogers@protonmail.com>
2018-08-17 11:25:49 +0200

    Expose fonts in index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { default as FlexGrid } from './grid/FlexGrid';
 export { default as Row } from './grid/Row';
 export { default as Col } from './grid/Col';
 export { default as Icon } from './icons/Icon';
+export { default as Fonts } from './fonts/fonts.style';
