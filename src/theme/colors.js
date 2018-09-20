eb6bfb515220d08d0954528f36fdc32ef4feaf1f
paschalidi <paschalidi@outlook.com>
2018-09-20 17:25:42 +0200

    cp/imports-textarea: imports Textarea component

---
+++ src/theme/colors.js
@@ -0,0 +1,23 @@
+const primary = "#03d4bf";
+const primaryHover = "#05e6c8";
+const error = "#fe5533";
+const gray = "#cdcdcd";
+const darkgray = "#4c4c4c";
+const headerColor = "#575756";
+const hint = "#898988";
+const primarybackground = "#fff";
+const secondarybackground = "#f7f7f7";
+const cardbackground = "#fefefe";
+
+export {
+  primary,
+  gray,
+  darkgray,
+  primarybackground,
+  secondarybackground,
+  error,
+  primaryHover,
+  headerColor,
+  hint,
+  cardbackground
+};
