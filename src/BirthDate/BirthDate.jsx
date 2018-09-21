817da929dfd18ed66f2a2d6675c76953389eaa9b
paschalidi <paschalidi@outlook.com>
2018-09-21 12:45:42 +0200

    cp/fixes-birthDate-component: removes if statement

--- src/BirthDate/BirthDate.jsx
+++ src/BirthDate/BirthDate.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export default function BirthDate({ date, style }) {
-  return date ? (
+  return (
     <div style={style}>
       {date.split("-")[2]}.{date.split("-")[1]}.{date.split("-")[0]}
     </div>
-  ) : null;
+  );
 }
 BirthDate.propTypes = {
   date: PropTypes.string.isRequired,
