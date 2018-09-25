89ee4cb86b6f26859c350ed8a2e57f1c9ec2d494
paschalidi <paschalidi@outlook.com>
2018-09-25 19:02:20 +0200

    cp/exports.component.simple.loader: corrects the default Name

--- src/SimpleLoader/SimpleLoader.jsx
+++ src/SimpleLoader/SimpleLoader.jsx
@@ -3,22 +3,22 @@ import PropTypes from "prop-types";
 import loading from "../../public/animations/loading.gif";
 import { AnimatedImg, Wrapper } from "./SimpleLoader.style";
 
-const Spinner = ({ width, height, centered }) => (
+const SimpleLoader = ({ width, height, centered }) => (
   <Wrapper centered={centered}>
     <AnimatedImg width={width} height={height} src={loading} alt="Loading..." />
   </Wrapper>
 );
 
-Spinner.propTypes = {
+SimpleLoader.propTypes = {
   width: PropTypes.number,
   height: PropTypes.number,
   centered: PropTypes.bool
 };
 
-Spinner.defaultProps = {
+SimpleLoader.defaultProps = {
   width: 50,
   height: 50,
   centered: false
 };
 
-export default Spinner;
+export default SimpleLoader;
