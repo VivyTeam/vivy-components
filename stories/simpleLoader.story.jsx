7e359c76f018455c81548070852786ef1e524dfb
paschalidi <paschalidi@outlook.com>
2018-09-25 19:17:54 +0200

    cp/exports.component.simple.loader: adds stories

--- stories/simpleLoader.story.jsx
+++ stories/simpleLoader.story.jsx
@@ -3,11 +3,28 @@ import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 import SimpleLoader from "../src/SimpleLoader";
 
-storiesOf("Animations", module).add(
-  "SimpleLoader",
-  withInfo("A simple loader")(() => (
-    <div>
-      <SimpleLoader centered />
-    </div>
-  ))
-);
+storiesOf("Animations", module)
+  .add(
+    "SimpleLoader",
+    withInfo("A simple loader")(() => (
+      <div>
+        <SimpleLoader />
+      </div>
+    ))
+  )
+  .add(
+    "SimpleLoader centered",
+    withInfo("A simple loader centered")(() => (
+      <div>
+        <SimpleLoader centered />
+      </div>
+    ))
+  )
+  .add(
+    "SimpleLoader with width and height",
+    withInfo("A simple loader")(() => (
+      <div>
+        <SimpleLoader width={100} height={100} />
+      </div>
+    ))
+  );
