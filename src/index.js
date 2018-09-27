36022c04a0bdb20117abc51ef651c79fe6d4195a
paschalidi <paschalidi@outlook.com>
2018-09-27 17:40:14 +0200

    cp/release.0.5.0: fixes exporting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-export { default as Button } from "./button/Button";
+export { default as Button } from "./button";
 export { default as FlexGrid } from "./grid/FlexGrid";
 export { default as Row } from "./grid/Row";
 export { default as Col } from "./grid/Col";
-export { default as Icon } from "./icons/Icon";
+export { default as Icon } from "./icons";
 export { default as Fonts } from "./fonts/fonts.style";
 export { default as BirthDate } from "./BirthDate";
 export { default as Textarea } from "./forms/Textarea";
