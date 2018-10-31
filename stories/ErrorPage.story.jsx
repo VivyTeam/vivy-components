6ceca4f5b4716d380859cf1d8a64ab2d5783c923
Herman (Boomer) Rogers <herman.rogers@protonmail.com>
2018-10-31 14:41:48 +0100

    Add 404 page component (#56)

---
+++ stories/ErrorPage.story.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { NotFoundPage, Topbar, Button } from "../src/index";
+import logo from "../public/images/logo.svg";
+
+storiesOf("Error Pages", module).add(
+  "404 page",
+  withInfo("A error page for when a route can't be found")(() => (
+    <div>
+      <Topbar>
+        <a
+          className="link"
+          href="https://www.vivy.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="logo" src={logo} alt="Vivy Logo" />
+        </a>
+      </Topbar>
+      <NotFoundPage>
+        <h2>Oops! Looks like we have lost that page.</h2>
+        <p>Please try again or return to the homepage</p>
+        <Button type="secondary">Return home</Button>
+      </NotFoundPage>
+    </div>
+  ))
+);
