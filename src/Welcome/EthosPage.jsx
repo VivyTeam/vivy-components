6142e6aa4ce90e19b5de41d44341b29a4a56a369
james-ciclitira <jciclitira@gmail.com>
2018-11-01 11:04:37 +0100

    principles-colors-update

--- src/Welcome/EthosPage.jsx
+++ src/Welcome/EthosPage.jsx
@@ -10,28 +10,16 @@ export default function WelcomePage() {
         <Col>
           <div className="container">
             <h1>Vision &amp; Ethos</h1>
-            <p>
-              This guide and component library aims to bring engineering and
-              design together. These components will directly tie into our
-              frontend infrastructure to empower a design first approach. With
-              this philosophy we aim to have consistent and accessible design
-              that unifies our features into a greater whole.
+            <p style={{ marginTop: "4rem" }}>
+              The styleguide and component library aims to bring engineering and
+              design together.
+              <br /> <br /> <br />
+              Components tie into our frontend infrastructure to empower a
+              design first approach.
+              <br /> <br /> <br />
+              With this philosophy we aim to have consistent and accessible
+              design that unifies our features into a greater whole.
             </p>
-            <br />
-            <h2 id="alt">Eat more fish, its healthy!</h2>
-            <p>―DR. TAMARA RADAKOVIC</p>
-            <br />
-            <h2 id="alt">So we use 2px or 4px for corner radius?</h2>
-            <p>―PIOTR ZIĘBA</p>
-            <br />
-            <h2 id="alt">When in doubt, crash the app.</h2>
-            <p>―MOHAMED ELAWADI</p>
-            <br />
-            <h2 id="alt">
-              Let the bugs stay there for a long time and they will become
-              butterfly
-            </h2>
-            <p>―JIUN WU</p>
           </div>
         </Col>
       </FlexGrid>
--- src/Welcome/welcome.style.js
+++ src/Welcome/welcome.style.js
@@ -1,45 +1,12 @@
 import styled from "styled-components";
 
 const WelcomeStyles = styled.div`
-  h1 {
-    color: #353f41;
-    font-size: 1.5em;
-  }
-
-  h2 {
-    color: #353f41;
-    font-size: 1.25em;
-  }
-
-  h3 {
-    color: #353f41;
-    font-size: 1em;
-  }
-
-  p {
-    color: #353f41;
-    font-size: 1em;
-    line-height: 1.38;
-    font-weight: 400;
-  }
-
-  #alt {
-    font-weight: 600;
-  }
-
-  img {
-    position: relative;
-  }
-
-  .caption {
-    font-size: 0.81em;
-    font-weight: 400;
-  }
-
   .container {
     background-color: #fff;
-    width: 50%;
+    width: 80%;
     margin: 0 auto;
+    margin-top: 4rem;
+    max-width: 32rem;
   }
 
   .box {
