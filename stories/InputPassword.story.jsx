c1d2d4588fd48071f6fb1ffafb7283f32e0f5331
paschalidi <paschalidi@outlook.com>
2018-12-06 14:33:56 +0100

    updates text

--- stories/InputPassword.story.jsx
+++ stories/InputPassword.story.jsx
@@ -11,7 +11,7 @@ const PageLayout = styled.div`
 storiesOf("InputPassword", module)
   .add(
     "icon left",
-    withInfo("default")(() => (
+    withInfo("Has an Icon on the left side.")(() => (
       <PageLayout>
         <InputPassword
           iconName="body"
@@ -24,8 +24,10 @@ storiesOf("InputPassword", module)
     ))
   )
   .add(
-    "with Visibility",
-    withInfo("initial state would not show what is typed in the input")(() => (
+    "with visibility property, true",
+    withInfo(
+      "the initial state of the input indicates that there will be an Icon on the right side. By clicking the button the input changes its type between 'text' and 'password'"
+    )(() => (
       <PageLayout>
         <InputPassword
           visibility
@@ -37,8 +39,10 @@ storiesOf("InputPassword", module)
     ))
   )
   .add(
-    "with Visibility and showPassword by default",
-    withInfo("initial state would show what is typed in the input")(() => (
+    "with visibility and showPassword properties, true",
+    withInfo(
+      "the initial state of the input indicates that there will be an Icon on the right side. Also the type of the Input will be 'text'. That means that when the user start typing will be able to see what he is typing in th password input."
+    )(() => (
       <PageLayout>
         <InputPassword
           visibility
