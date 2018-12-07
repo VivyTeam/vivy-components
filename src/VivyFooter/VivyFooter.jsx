4c292aa1244a6d45e00cc9488a6f1bb3d9104d73
paschalidi <paschalidi@outlook.com>
2018-12-07 13:18:24 +0100

    footer

--- src/VivyFooter/VivyFooter.jsx
+++ src/VivyFooter/VivyFooter.jsx
@@ -5,40 +5,44 @@ import { Col, Row, Footer } from "../index";
 export default function VivyFooter() {
   return (
     <Footer>
-      <Styles>
-        <Row position="center" verticalAlign="middle">
-          <Col lg={0}>
-            <a
-              className="anchor"
-              href="https://www.vivy.com/impressum"
-              target="_"
-              rel="noopener noreferrer"
-            >
-              Impressum
-            </a>
-          </Col>
-          <Col lg={0}>
-            <a
-              className="anchor"
-              href="https://www.vivy.com/nutzungsbedingungen"
-              target="_"
-              rel="noopener noreferrer"
-            >
-              AGB
-            </a>
-          </Col>
-          <Col lg={0}>
-            <a
-              className="anchor last"
-              href="https://www.vivy.com/datenschutz"
-              target="_"
-              rel="noopener noreferrer"
-            >
-              Datenschutz
-            </a>
-          </Col>
-        </Row>
-      </Styles>
+      <Row>
+        <Col lg={12}>
+          <Styles>
+            <Row position="center" verticalAlign="middle">
+              <Col lg={0}>
+                <a
+                  className="anchor"
+                  href="https://www.vivy.com/impressum"
+                  target="_"
+                  rel="noopener noreferrer"
+                >
+                  Impressum
+                </a>
+              </Col>
+              <Col lg={0}>
+                <a
+                  className="anchor"
+                  href="https://www.vivy.com/nutzungsbedingungen"
+                  target="_"
+                  rel="noopener noreferrer"
+                >
+                  AGB
+                </a>
+              </Col>
+              <Col lg={0}>
+                <a
+                  className="anchor last"
+                  href="https://www.vivy.com/datenschutz"
+                  target="_"
+                  rel="noopener noreferrer"
+                >
+                  Datenschutz
+                </a>
+              </Col>
+            </Row>
+          </Styles>
+        </Col>
+      </Row>
     </Footer>
   );
 }
