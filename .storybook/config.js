792b48e354550d38c75c29884072c0199a0a1118
paschalidi <paschalidi@outlook.com>
2019-01-12 23:35:29 +0100

    adds babel-plugin-styled-component

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,6 @@
 import { addDecorator, configure } from "@storybook/react";
-import { withOptions } from '@storybook/addon-options';
-import { withInfo } from '@storybook/addon-info';
+import { withOptions } from "@storybook/addon-options";
+import { withInfo } from "@storybook/addon-info";
 
 addDecorator(withInfo);
 addDecorator(
