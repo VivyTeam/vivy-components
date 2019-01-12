377b30c02b92d338e5055551df5b9c2b3350e9cc
paschalidi <paschalidi@outlook.com>
2019-01-12 21:07:19 +0100

    replace setOptions with withOptions because of depracated

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,14 @@
-import { configure } from "@storybook/react";
-import { setOptions } from "@storybook/addon-options";
+import { addDecorator, configure } from "@storybook/react";
+import { withOptions } from '@storybook/addon-options';
 
-setOptions({
-  name: "Vivy",
-  url: "https://www.vivy.com/",
-  showStoriesPanel: true,
-  showAddonPanel: true
-});
+addDecorator(
+  withOptions({
+    name: "Vivy",
+    url: "https://www.vivy.com/",
+    showStoriesPanel: true,
+    showAddonPanel: true
+  })
+);
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../stories", true, /.stories.js$/);
