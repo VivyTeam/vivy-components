20bf17ec1de65c0541510526ff6c07629118ae65
paschalidi <paschalidi@outlook.com>
2019-01-16 17:08:34 +0100

    *updates storybook *sorts out packages
    removes unused addon
    
    
    more cleanup
    
    
    package
    
    
    automated snapshots for all stories
    
    
    replace setOptions with withOptions because of depracated
    
    
    removes with info as it is depracated
    
    
    adds babel-plugin-styled-component
    
    
    removes coverage
    
    
    lock
    
    
    react-scripts
    
    
    mocks static assets
    
    
    removes less
    
    
    updates with npx babel-upgrade --write
    
    
    update script
    
    
    
    adds test check
    
    
    adds test check in the correct place
    
    
    notify with info for v4
    
    
    npm test
    
    
    fixes console.error
    
    
    updates snapshots
    
    
    Update notify text styles (#160)
    
    * Update notify text styles
    
    * Remove text-align
    
    0.7.9
    
    0.7.11
    
    updates styled-components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,16 +1,11 @@
+import React from "react";
 import { addDecorator, configure } from "@storybook/react";
-import { withOptions } from "@storybook/addon-options";
 import { withInfo } from "@storybook/addon-info";
+import { withGlobalStyles, withVivyOptions } from "./customDecorators";
 
+addDecorator(withGlobalStyles);
+addDecorator(withVivyOptions);
 addDecorator(withInfo);
-addDecorator(
-  withOptions({
-    name: "Vivy",
-    url: "https://www.vivy.com/",
-    showStoriesPanel: true,
-    showAddonPanel: true
-  })
-);
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../stories", true, /.stories.js$/);
---
+++ .storybook/customDecorators.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Fonts from "../src/Fonts/fonts.style";
+import { withOptions } from "@storybook/addon-options";
+
+export function withGlobalStyles(storyFn) {
+  return (
+    <React.Fragment>
+      <Fonts />
+      {storyFn()}
+    </React.Fragment>
+  );
+}
+
+export const withVivyOptions = withOptions({
+    name: "Vivy",
+    url: "https://www.vivy.com/",
+    showStoriesPanel: true,
+    showAddonPanel: true
+  })
