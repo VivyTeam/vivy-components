b90d4d4173a8ab869bd4b6d748ae6bc465f15a01
paschalidi <paschalidi@outlook.com>
2019-01-22 14:30:25 +0100

    updates images
    updates images

--- tests/storyshotsPuppeteer.test.js
+++ tests/storyshotsPuppeteer.test.js
@@ -1,12 +1,19 @@
 import initStoryshots from "@storybook/addon-storyshots";
 import { imageSnapshot } from "@storybook/addon-storyshots-puppeteer";
+import devices from "puppeteer/DeviceDescriptors";
 import { failureThreshold, getStorybookUrl } from "./helpers";
+
+const tablet = devices["iPad landscape"];
+const customizePage = page => page.emulate(tablet);
+const beforeScreenshot = page => page.emulate(tablet);
 
 initStoryshots({
   suite: "Image storyshots",
   framework: "react",
   test: imageSnapshot({
     storybookUrl: getStorybookUrl(),
-    getMatchOptions: failureThreshold
+    getMatchOptions: failureThreshold,
+    customizePage,
+    beforeScreenshot
   })
 });
