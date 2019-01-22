49326700b51988739fd8eb46132fa9ef645e8338
paschalidi <paschalidi@outlook.com>
2019-01-22 09:31:11 +0100

    cleans from enzyme

--- tests/storyshots.test.js
+++ tests/storyshots.test.js
@@ -1,8 +1,4 @@
 import "jest-styled-components";
 import initStoryshots from "@storybook/addon-storyshots";
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-configure({ adapter: new Adapter() });
 
 initStoryshots({ framework: "react" });
