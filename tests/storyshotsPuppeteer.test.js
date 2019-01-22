b591ecbf1681b6028950a459ef0d42879c79ac04
paschalidi <paschalidi@outlook.com>
2019-01-22 14:36:15 +0100

    cleans

--- tests/storyshotsPuppeteer.test.js
+++ tests/storyshotsPuppeteer.test.js
@@ -1,11 +1,11 @@
 import initStoryshots from "@storybook/addon-storyshots";
 import { imageSnapshot } from "@storybook/addon-storyshots-puppeteer";
-import devices from "puppeteer/DeviceDescriptors";
-import { failureThreshold, getStorybookUrl } from "./helpers";
-
-const tablet = devices["iPad landscape"];
-const customizePage = page => page.emulate(tablet);
-const beforeScreenshot = page => page.emulate(tablet);
+import {
+  failureThreshold,
+  getStorybookUrl,
+  customizePage,
+  beforeScreenshot
+} from "./helpers";
 
 initStoryshots({
   suite: "Image storyshots",
