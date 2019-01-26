b10f0ecc722e73c806a39c297861f164936e65f6
paschalidi <paschalidi@outlook.com>
2019-01-26 16:24:57 +0100

    removes emoji

--- dangerfile.js
+++ dangerfile.js
@@ -6,7 +6,7 @@ const { modified_files: modifiedFiles } = danger.git;
 
 const bigPRThreshold = 700;
 if (pr.additions - pr.deletions > bigPRThreshold) {
-  const title = ":exclamation: Big PR";
+  const title = "Big PR";
   const idea = `This PR is unlikely to get reviewed because it touches too many lines 
   (${pr.additions - pr.deletions}). 
   Consider sending smaller Pull Requests and stack them on top of each other.`;
@@ -18,7 +18,7 @@ if (pr.additions - pr.deletions > bigPRThreshold) {
 }
 
 if (pr.requested_teams.length === 0 && pr.requested_reviewers.length === 0) {
-  const title = ":exclamation: Missing reviewer";
+  const title = "Missing reviewer";
   const idea =
     "Looks like this pull request is missing a reviewer. That's OK as long as this PR is still a 'work in progress', if that is the case please add 'WIP' in the PR title or corresponding label.";
   warn(`${title} - <i>${idea}</i>`);
@@ -29,7 +29,7 @@ if (pr.requested_teams.length === 0 && pr.requested_reviewers.length === 0) {
 }
 
 if (pr.body.length < 10) {
-  const title = ":exclamation: Missing description";
+  const title = "Missing description";
   const idea =
     "Help your reviewers by writing a paragraph or two with the goals and motives of this PR.";
   warn(`${title} - <i>${idea}</i>`);
