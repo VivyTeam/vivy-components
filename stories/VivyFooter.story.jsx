dc4daee0bfdaf8da34c6f61522805a5136ffcc9d
paschalidi <paschalidi@outlook.com>
2019-02-06 10:19:46 +0100

    moves margin to the story not the footer itself

--- stories/VivyFooter.story.jsx
+++ stories/VivyFooter.story.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { VivyFooter } from "../src/index";
 
-storiesOf("VivyFooter", module).add("default styles", () => <VivyFooter />, {
-  info: "The raw footer component"
-});
+storiesOf("VivyFooter", module).add(
+  "default styles",
+  () => (
+    <div style={{ marginTop: 100 }}>
+      <VivyFooter />
+    </div>
+  ),
+  { info: "The raw footer component" }
+);
