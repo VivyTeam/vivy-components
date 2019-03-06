d46040bb44805536a71bad28ec46e5acaaf2727c
John Hannagan <weetbix_@hotmail.com>
2019-03-06 12:13:23 +0100

    Bump React-Storybook to 5 (#253)
    * Upgrade storybook and dependencies to v5
    
    * Update storyshot snapshot for new v5 layout
    
    * Remove unused addon-links
    
    * Remove depricated options addon, replace with new setup

--- .storybook/customDecorators.js
+++ .storybook/customDecorators.js
@@ -1,13 +1,5 @@
-import { withOptions } from "@storybook/addon-options";
 import { Fonts, GlobalStyles } from "../src/index";
 import React from "react";
-
-export const withVivyOptions = withOptions({
-  name: "Vivy",
-  url: "https://www.vivy.com/",
-  showStoriesPanel: true,
-  showAddonPanel: false
-});
 
 export function withGlobalStyles(storyFn) {
   return (
