b7b5ca0a037d2c66a5757923813754f6bb5683ee
John Hannagan <weetbix_@hotmail.com>
2019-04-03 16:12:47 +0200

    Update src/Display/Display.jsx
    Co-Authored-By: paschalidi <paschalidi@outlook.com>

--- src/Display/Display.jsx
+++ src/Display/Display.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-// This is a div element that is in font-size bigger than h1 and is supposed to
+// This is a div element that is in font-size bigger than h1 and is supposed to be
 // used as 'slogan' around our apps. We dont want this to be crawled from search engines
 // since its only for design reasons but instead we want the h1 to be so as it is meant by default.
 const DisplayStyles = styled.div`
