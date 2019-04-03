cbec152d46a172920140068497c44277ceebb016
christos <paschalidi@outlook.com>
2019-04-03 15:59:24 +0200

    renames stories

--- stories/H1.story.jsx
+++ stories/H1.story.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { H1, Colors } from "../src/index";
 
-storiesOf("HeadingOne", module)
+storiesOf("H1", module)
   .add("basic", () => <H1>Better Health Needs Vivy</H1>)
   .add("styled", () => (
     <H1 style={{ color: Colors.brandPrimary }}>Better Health Needs Vivy</H1>
--- stories/H2.story.jsx
+++ stories/H2.story.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { H2, Colors } from "../src/index";
 
-storiesOf("HeadingTwo", module)
+storiesOf("H2", module)
   .add("basic", () => <H2>Better Health Needs Vivy</H2>)
   .add("styled", () => (
     <H2 style={{ color: Colors.brandPrimary }}>Better Health Needs Vivy</H2>
