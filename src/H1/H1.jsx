d6119481951d2f846ca5818ff6afe9c05394ceb2
christos <paschalidi@outlook.com>
2019-04-04 14:59:36 +0200

    rem for h1

--- src/H1/H1.jsx
+++ src/H1/H1.jsx
@@ -4,8 +4,8 @@ import styled from "styled-components";
 
 const Styles = styled.h1`
   font-weight: 500;
-  font-size: 2.25rem;
-  line-height: 2.625rem;
+  font-size: 3.6rem;
+  line-height: 4.2rem;
 `;
 
 export default function H1({ children, style }) {
