0092bf84ad7afd9c358413776b5627ec991c8b5b
christos <paschalidi@outlook.com>
2019-04-10 17:34:01 +0200

    zindex is a var

--- src/Modal/modal.style.js
+++ src/Modal/modal.style.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
+import { Colors } from "../index";
 
+const zIndex = 10;
 export const Overlay = styled.div`
   position: absolute;
   left: 0;
   top: 0;
   width: 100%;
   height: 100%;
-  z-index: 10; // This must be at a higher index to the rest of your page content
-  transform: translateZ(0);
+  z-index: ${zIndex};
   background-color: rgba(0, 0, 0, 0.7);
 `;
 
@@ -17,20 +18,17 @@ export const ModalStyles = styled.div`
   top: 0;
   width: 100%;
   height: 100%;
-  z-index: 10;
-  overflow-y: auto;
-  -webkit-overflow-scrolling: touch;
+  z-index: ${zIndex};
 `;
 
 export const Content = styled.div`
-  z-index: 10;
+  z-index: ${zIndex};
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   max-height: 90%;
-  overflow: auto;
-  background: #fff;
+  background: ${Colors.white};
   padding: 20px;
   box-shadow: 0 1px 5px rgba(0, 0, 0, 0.7);
   text-align: center;
