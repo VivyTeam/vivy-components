e018cab4b8b6a3d2f219fc6599934f9ddb2070a7
christos <paschalidi@outlook.com>
2019-04-12 10:40:16 +0200

    ✅  When open, focus is drawn immediately to the Modal's close button

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
-import { ModalStyles, Overlay, Content } from "./modal.style";
+import { disableBodyScroll, clearAllBodyScrollLocks } from "body-scroll-lock";
+import { ModalStyles, Overlay, Content, CloseButton } from "./modal.style";
 
 class Modal extends Component {
+  closeButton = React.createRef();
+
   componentDidMount() {
+    this.focusOnCloseButton();
     disableBodyScroll(document.querySelector("body"));
   }
 
@@ -12,12 +15,25 @@ class Modal extends Component {
     enableBodyScroll(document.querySelector("body"));
   }
 
+  focusOnCloseButton = () => {
+    this.closeButton.current.focus();
+  };
+
   render() {
     const { onClose, children } = this.props;
     return (
       <ModalStyles>
-        <Overlay onClick={onClose} />
-        <Content>{children}</Content>
+        <Overlay onClick={closeCallback} />
+        <Content>
+          <Row>
+            <Col offset={11} lg={1}>
+              <CloseButton ref={this.closeButton} onClick={closeCallback}>
+                <Icon name="close" />
+              </CloseButton>
+            </Col>
+          </Row>
+          {children}
+        </Content>
       </ModalStyles>
     );
   }
