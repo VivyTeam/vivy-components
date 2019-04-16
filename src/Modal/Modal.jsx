a858528f8e2f6cff34784024935cdd039287e3c9
Christos Paschalidis <paschalidi@outlook.com>
2019-04-16 15:27:18 +0200

    MAIN -  adds accessibility features  on the modal (#338)
    * for the sake of history
    
    * now reverts
    
    * ✅  When open, focus is drawn immediately to the Modal's close button
    
    * ✅  Focus is trapped within the Modal when open.
    
    * cleanup
    
    * comment
    
    * snaps
    
    * height and width are now defined
    
    * adds snapshots
    
    * on close
    
    * onClose
    
    * styles button accordingly
    
    * snapshots
    
    * moves styles into body
    
    * snapshots
    
    * onClose

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import ReactFocusTrap from "react-focus-lock";
 import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
-import { ModalStyles, Overlay, Content } from "./modal.style";
+import {
+  ModalStyles,
+  Overlay,
+  Content,
+  CloseButton,
+  BodyContent
+} from "./modal.style";
+import { Row, Col, Icon } from "../index";
 
 class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.closeButton = React.createRef();
+  }
+
   componentDidMount() {
+    if (this.closeButton.current) {
+      this.closeButton.current.focus();
+    }
     disableBodyScroll(document.querySelector("body"));
   }
 
@@ -13,12 +29,29 @@ class Modal extends Component {
   }
 
   render() {
-    const { onClose, children } = this.props;
+    const { onClose, role, ariaLabel, children } = this.props;
     return (
-      <ModalStyles>
-        <Overlay onClick={onClose} />
-        <Content>{children}</Content>
-      </ModalStyles>
+      <ReactFocusTrap
+        tag="aside"
+        focusTrapOptions={{ onDeactivate: onClose }}
+        aria-modal="true"
+        role={role}
+        aria-label={ariaLabel}
+      >
+        <ModalStyles>
+          <Overlay onClick={onClose} />
+          <Content>
+            <Row textAlign="right">
+              <Col offset={11} lg={1}>
+                <CloseButton ref={this.onClose} onClick={onClose}>
+                  <Icon name="close" />
+                </CloseButton>
+              </Col>
+            </Row>
+            <BodyContent>{children}</BodyContent>
+          </Content>
+        </ModalStyles>
+      </ReactFocusTrap>
     );
   }
 }
@@ -28,10 +61,14 @@ Modal.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
   ]).isRequired,
-  onClose: PropTypes.func
+  onClose: PropTypes.func,
+  role: PropTypes.string,
+  ariaLabel: PropTypes.string
 };
 Modal.defaultProps = {
-  onClose: () => {}
+  onClose: () => {},
+  role: "dialog",
+  ariaLabel: "" // A Label for the Modal that describes what it is.
 };
 
 export default Modal;
--- src/Modal/modal.style.js
+++ src/Modal/modal.style.js
@@ -13,6 +13,28 @@ export const Overlay = styled.div`
   background-color: #000000;
 `;
 
+export const CloseButton = styled.button`
+  display: inline-flex;
+  background: none;
+  color: inherit;
+  border: none;
+  font: inherit;
+  cursor: pointer;
+  padding: 0;
+
+  i {
+    font-size: 24px;
+    width: 24px;
+    height: 24px;
+    text-align: center;
+    color: #575756;
+  }
+
+  div {
+    display: inline-flex;
+  }
+`;
+
 export const ModalStyles = styled.div`
   position: fixed;
   left: 0;
@@ -30,8 +52,13 @@ export const Content = styled.div`
   transform: translate(-50%, -50%);
   max-height: 90%;
   background: ${Colors.white};
-  padding: 24px;
+  padding: 16px 16px 24px 16px;
   text-align: center;
   border-radius: 8px;
   min-width: 320px; /* This just a default width */
+`;
+
+export const BodyContent = styled.div`
+  margin-left: 24px;
+  margin-right: 24px;
 `;
