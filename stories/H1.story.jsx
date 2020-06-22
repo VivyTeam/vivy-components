642d9ca52b26efbfd8760349f0447dace3a5a8b1
Ahmed Hassanein <ahmed.hassanein@vivy.com>
2020-06-22 17:44:06 +0200

    REVERT

--- stories/H1.story.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { H1, Colors } from "../src/index";
-
-storiesOf("H1", module)
-  .add("basic", () => <H1>Better Health Needs Vivy</H1>)
-  .add("styled", () => (
-    <H1 style={{ color: Colors.primary }}>Better Health Needs Vivy</H1>
-  ));
--- stories/H2.story.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { H2, Colors } from "../src/index";
-
-storiesOf("H2", module)
-  .add("basic", () => <H2>Better Health Needs Vivy</H2>)
-  .add("styled", () => (
-    <H2 style={{ color: Colors.primary }}>Better Health Needs Vivy</H2>
-  ));
