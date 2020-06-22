6a6099647d323899c53ccc32f667f8ed46ba8b3c
Ahmed Hassanein <ahmed.hassanein@vivy.com>
2020-06-22 14:51:55 +0200

    fix snapshots

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,16 +1,8 @@
-import path from "path";
-import fs from "fs";
 import devices from "puppeteer/DeviceDescriptors";
 
 export function getStorybookUrl() {
-  let location = path.join(__dirname, "..", "storybook-static");
 
-  if (fs.existsSync(location)) {
-    location = `file://${location}`;
-  } else {
-    location = "http://localhost:3006";
-  }
-  return location;
+  return "http://localhost:3006";
 }
 
 export const failureThreshold = () => ({
