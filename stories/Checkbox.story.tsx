92ae42f056974e0881929a09e11fe7623476f0ea
Fabian <wulf.fabian@gmail.com>
2020-07-08 14:27:05 +0200

    Update story and storyshot tests
    * Rename story to align with other stories
    * Remove validation story because of current testing and possibility of non exemption
    * Update tests

--- stories/Checkbox.story.tsx
+++ stories/Checkbox.story.tsx
@@ -1,20 +1,9 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { Checkbox, Validation } from "../src";
-import { ValidationRules } from "../src/Forms/Validation";
-
-const validationRules: ValidationRules = {
-  newsletter: {
-    type: "enum",
-    enum: ["true"],
-    required: true,
-    message: "*You must agree to the newsletter",
-    transform: (value = false) => value.toString(),
-  },
-};
+import { Checkbox } from "../src";
 
 storiesOf("Checkbox", module)
-  .add("default", () => (
+  .add("basic", () => (
     <Checkbox id="newsletter" name="newsletter">
       Sign up newsletter
     </Checkbox>
@@ -23,11 +12,4 @@ storiesOf("Checkbox", module)
     <Checkbox id="newsletter" name="newsletter" optional>
       Sign up newsletter
     </Checkbox>
-  ))
-  .add("with error validation", () => (
-    <Validation rules={validationRules}>
-      <Checkbox id="newsletter" name="newsletter">
-        Sign up newsletter
-      </Checkbox>
-    </Validation>
   ));
